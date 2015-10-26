1d83664897e2a182acf11bc086ccbfca8dded67f
Kexone <kami.extreme@gmail.com>
2015-10-27 00:56:36 +0100

    Revert "Bordelized"
    This reverts commit d2312407a23165d53cf59ada9b7857ce9ec6fd1c.

--- ZPG1/AbstractObserver.h
+++
@@ -1,9 +0,0 @@
-#include <glm/mat4x4.hpp> // glm::mat4
-
-class AbstractObserver {
-
-public: 
-	//virtual void onProjectionChanged(glm::mat4 ProjectionMaxtrix) = 0;
-	//virtual void onViewChanged(glm::mat4 ViewMatrix) = 0;
-	virtual void update(glm::vec3 vector) = 0;
-};
